203ac147fb24e02d01b5fc421dbde784bad73db6
Anderson Rodrigues <anddersonrds@gmail.com>
2020-08-03 21:41:48 -0300

    add routes

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -28,15 +29,15 @@ const Landing = () => {
         <HeroImage src={landingImg} alt="Sua plataforma de estudos" />
 
         <ButtonsContainer>
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Estudar" />
             Dar aulas
-          </a>
+          </Link>
         </ButtonsContainer>
         <TotalConnections>
           Total de 200 conexões já realizadas
